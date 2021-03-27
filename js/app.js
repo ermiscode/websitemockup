a4d34cbd3865bb558fe5f4c5c2cb3d044f861364
Ermis-codes <ermismcarter@gmail.com>
2021-03-27 14:58:27 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const menuIcon = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+  navbar.classList.toggle('change');
+})
+
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.slider');
+const logo = document.querySelector('#logo');
+const headline = document.querySelector('.headline');
+
+const tl = new TimelineMax();
+
+tl.fromTo(hero,1,
+  {height: "0%"},
+  {height: "80%", ease: Power2.easeInOut }
+).fromTo(
+  hero,
+  1.2,
+  {width: "100%"},
+  {width: "80%", ease: Power2.easeInOut }
+)
+.fromTo(slider, 1.2,{x: "-100%"}, {x: "0%" ,ease: Power2.easeInOut }, "-=1.2")
